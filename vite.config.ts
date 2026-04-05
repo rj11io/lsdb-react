00334d3f61d1716a7f7a1a2101e433ae02edb4ec
Ricardo Jorge <ricardojorgexyz@gmail.com>
2026-04-05 02:22:29 +0100

    fix: test ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
-import { resolve } from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  root: resolve(__dirname, "tests/app"),
-  resolve: {
-    alias: {
-      "@rj11io/lsdb": resolve(__dirname, "../lsdb/src/index.ts"),
-    },
-  },
+  root: new URL("./tests/app", import.meta.url).pathname,
   server: {
     host: "127.0.0.1",
     port: 4173,
